b2a733671c43e1d5c041acb7e7e92fe6ee0c4dfa
Miika Savela <miika.a.savela@gmail.com>
2023-08-26 09:38:49 +0300

    Fix footer logo not being centered on large screen sizes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 </main>
 <footer class="bg-stone-950 text-stone-50">
 	<div class="px-4 py-6 container mx-auto">
-		<img src={logo_white} alt="Logo of Fjäder media" class="max-h-[120px] mx-auto md:mx-0 mb-8" />
+		<img src={logo_white} alt="Logo of Fjäder media" class="max-h-[120px] mx-auto mb-8" />
 		<p class="text-sm text-center">Videoita tarinoista ja ihmisistä jotka jäävät mieleen</p>
 	</div>
 </footer>
